dae7e5146d7a14c4343d50a4e978dd804b74d569
labile <1a6i1e@gmail.co>
2021-11-25 13:21:50 +0300

    update for new component changes

--- App/Command/Compatibility.php
+++ App/Command/Compatibility.php
@@ -8,16 +8,15 @@ use Astaroth\Attribute\ClientInfo;
 use Astaroth\Attribute\Conversation;
 use Astaroth\Attribute\Event\MessageNew;
 use Astaroth\Commands\BaseCommands;
-use Astaroth\DataFetcher\Events\MessageNew as Data;
 
 #[Conversation(Conversation::PERSONAL_DIALOG)]
 #[MessageNew]
 class Compatibility extends BaseCommands
 {
     #[ClientInfo([], keyboard: false, inline_keyboard: false, carousel: false)]
-    public function keyboardIncompatability(Data $data): bool
+    public function keyboardIncompatability(): bool
     {
-        $this->message($data->getPeerId(), "Твой клиент не поддерживает современные функции вконтакте, обновись!");
+        $this->message("Твой клиент не поддерживает современные функции вконтакте, обновись!");
         return false;
     }
 }
